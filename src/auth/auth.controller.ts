7d2310a451d302fec62bf19132087b85efe1ea1a
oscaralexander.taures <oscaralexander.taures@salud.gob.sv>
2025-06-19 15:27:03 -0600

    current

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { AuthService } from './auth.service';
+import { Get, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from './guard/auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+  
+  @Get('profile')
+  @UseGuards(AuthGuard)
+  profile(@Request() req) {
+    return 'profile';
+  }
+}
